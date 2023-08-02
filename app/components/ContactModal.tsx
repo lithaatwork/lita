1f9dff55d7ce3c9a97e98bd77cfa5bfc8d3dffe3
de-mawo <groovebassist1@gmail.com>
2023-08-02 16:08:14 +0200

    Fixed the contacts modal

--- app/components/ContactModal.tsx
+++ app/components/ContactModal.tsx
@@ -20,21 +20,21 @@ const ContactModal = ({ title }: Props) => {
       <Modal isOpen={isOpen} closeModal={closeModal}>
         <div className="flex flex-col space-y-6 p-4 bg-blue-100 rounded-md">
           <div className="flex items-center justify-between">
-            <p>+27 21 5101461</p>
+          <a href="tel:275101461">+27 21 5101461</a>
 
             <a href="tel:275101461">
               <HiPhone size={24} />
             </a>
             </div>
             <div className="flex items-center justify-between">
-              <p>info@lithaatwork.co.za</p>
+            <a href="mailto:info@lithaatwork.co.za">info@lithaatwork.co.za</a>
 
               <a href="mailto:info@lithaatwork.co.za">
                 <HiMail size={24} />
               </a>
             </div>
             <div className="flex items-center justify-between">
-              <p>+27 71 906 7680</p>
+              <a href="https://wa.me/27719067680?text=I'm%20inquiring%20about%20your%20services"> +27 71 906 7680</a>
 
               <a href="https://wa.me/27719067680?text=I'm%20inquiring%20about%20your%20services">
                 <RiWhatsappFill size={24} />
